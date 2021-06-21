3cbeb74d1b22c35309f338bd63be622018b69398
Abel Lee <36790966+abelleeye@users.noreply.github.com>
2021-06-21 19:03:34 +0800

    Update crazy_use_gpu.cu

--- crazy_use_gpu.cu
+++ crazy_use_gpu.cu
@@ -8,7 +8,7 @@ const int hdelay = 1000;
 __global__ void dkern(){
 
   long long start = clock64();
-  while(clock64() < start+tdelay);
+  while(clock64() < start + tdelay);
 }
 
 int main(int argc, char *argv[]){
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]){
   int my_delay = hdelay;
   if (argc > 1) my_delay = atoi(argv[1]);
   while (i == 0){
-    dkern<<<1,1>>>();
+    dkern<<<1, 1>>>();
     usleep(my_delay);
   }
   return 0;
