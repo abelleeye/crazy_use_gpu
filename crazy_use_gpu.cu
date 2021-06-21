9b1cd7e0ecb36e8ebbbf381c395c212f302706f7
Abel Lee <36790966+abelleeye@users.noreply.github.com>
2021-06-21 19:01:05 +0800

    Create crazy_use_gpu.cu

---
+++ crazy_use_gpu.cu
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+const long long tdelay = 1000000LL;
+const int hdelay = 1000;
+
+__global__ void dkern(){
+
+  long long start = clock64();
+  while(clock64() < start+tdelay);
+}
+
+int main(int argc, char *argv[]){
+  int i = 0;
+  int my_delay = hdelay;
+  if (argc > 1) my_delay = atoi(argv[1]);
+  while (i == 0){
+    dkern<<<1,1>>>();
+    usleep(my_delay);
+  }
+  return 0;
+}
